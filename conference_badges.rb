cbc67874006c7c02f8282c117e3b79377bd782de
Derek Haller <dhaller.personal@gmail.com>
2018-01-25 05:49:33 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,8 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  msg_array = []
+  array.each do |name|
+    msg_array.push("Hello, my name is #{name}.")
