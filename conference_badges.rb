133998bc6ed1f830641e8e532857e4138a246ccc
Derek Haller <dhaller.personal@gmail.com>
2018-01-25 06:04:35 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,6 +22,12 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  puts batch_badge_creator(array)
+  batch_badge_creator(array) = batch_array
+  batch_array.each do |msg|
+    puts "#{msg}"
+  end
+  assign_rooms(array) = room_array
+  room_array.each do |rm|
+    puts "#{rm}"
   puts assign_rooms(array)
 end
